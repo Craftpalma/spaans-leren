594528071e7867b32eaa853f96c301f3722e2f31
Craftpalma <50376513+Craftpalma@users.noreply.github.com>
2026-09-22 21:39:45 +0200

    Update calendario.js

--- calendario.js
+++ calendario.js
@@ -54,14 +54,12 @@ function renderCalendar() {
     const firstDay = new Date(year, month, 1);
 
     let startingDay = firstDay.getDay();
-      startingDay = (startingDay + 6) % 7);
-    // const startingDay = (new Date(year, month, 1).getDay() + 6) % 7;
     // Convertimos domingo = 0 a lunes = 0
-   /* if (startingDay === 0) {
+    if (startingDay === 0) {
         startingDay = 6;
     } else {
         startingDay = startingDay - 1;
-    }*/
+    }
    
     const daysInMonth = new Date(year, month + 1, 0).getDate();
 
